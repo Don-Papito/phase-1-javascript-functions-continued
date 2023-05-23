2cf425b9e92f1271496293044258a41c6cadd4e0
Steve <sjd734312@gmail.com>
2023-05-23 11:02:49 -0400

    Functionpart II

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun());
+
+  function mondayWork(workPlace = "go to the office"){
+    return `This Monday, I will ${workPlace}.`;
+  }
+  
+  mondayWork("work from home");
+
+  function wrapAdjective(){
+    //
+  }
+
+  function wrapAdjective(spice = "*"){
+    return function(description = "special"){
+        return `You are ${spice}${description}${spice}!`;
+    };
+  }
+
+const encouragingPromptFunction = wrapAdjective("!!!");
